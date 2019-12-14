c629a4911b1e3e5d81f8b5f4d9603c97a16ac1c5
Christofferos <42782387+Christofferos@users.noreply.github.com>
2019-12-15 00:16:21 +0100

    Functionality implemented: Add, Remove, Increment/Decrement from Cart, Calculate the cost for the devices.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Default from "./components/Default";
+import Cart from "./components/Cart";
+import Modal from "./components/Modal";
 
 class App extends Component {
   render() {
     return (
-      <div class="container">
-        <div class="row">
-          <div class="col-6">Col one</div>
-          <div class="col-6">Col two</div>
-        </div>
-      </div>
+      <React.Fragment>
+        <Navbar></Navbar>
+        <Switch>
+          <Route exact path="/" component={ProductList}></Route>
+          <Route path="/details" component={Details}></Route>
+          <Route path="/cart" component={Cart}></Route>
+          <Route component={Default}></Route>
+        </Switch>
+        <Modal></Modal>
+      </React.Fragment>
     );
   }
 }
